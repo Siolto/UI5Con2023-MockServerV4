1baeee63cb82f87d3e9e4a8ea578a5470ba6603e
Siolto <simon.coen@js-soft.com>
2023-07-05 16:24:13 +0200

    chore: prepare for presentation

--- webapp/localService/mockdata/EntityContainer.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    executeAction: async function (actionDefinition, actionData, keys) {
-        // this.throwError("my super error message!", 401)
-        const employeeEntity = await this.base.getEntityInterface("Employees")
-        await employeeEntity.addEntry({ EmployeeID: "10", FirstName: "UI5Con", LastName: "2023" })
-    }
-};
